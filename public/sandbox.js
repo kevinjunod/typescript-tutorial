8895a8d945c5e7bfc1a4dee78fa1acab875377ea
Kevin Junod <kevin@terralogiq.com>
2021-08-29 22:50:37 +0700

    Typescript Web demo

---
+++ public/sandbox.js
@@ -0,0 +1,9 @@
+"use strict";
+var greet;
+greet = function () {
+    console.log('hello agian');
+};
+var add = function (a, b, c) {
+    console.log(a + b);
+};
+add(1644, 993);
---
+++ source/sandbox.ts
@@ -0,0 +1,11 @@
+let greet: Function;
+
+greet = () => {
+    console.log('hello agian');
+}
+
+const add = (a: number, b: number, c?: number | string) => {
+    console.log(a + b);
+}
+
+add(1644, 993)
